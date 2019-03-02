08a0219f0d4045be58fa59b8019ea20168fb3281
Euan Goddard <euan@stockopedia.com>
2019-03-02 11:41:49 +0000

    Added handling of space and enter keys to trigger update

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { buildWord } from './words';
 import { Shake } from './shake';
+import { Strings } from './models';
+import { debounce } from 'lodash-es';
+
+const RELEVANT_KEYBOARD_EVENTS: Strings = ['Space', 'Enter'];
 
 (function() {
   updateWord();
@@ -10,10 +14,24 @@ import { Shake } from './shake';
   shaker.start();
 
   window.addEventListener('shake', updateWord, false);
+  window.addEventListener(
+    'keypress',
+    debounce(checkKeyEvent, 100, {
+      leading: true,
+      trailing: false,
+    }),
+    false,
+  );
 })();
 
 function updateWord(): void {
   const word = buildWord();
   const element = document.querySelector('h1') as HTMLElement;
   element.textContent = word;
+}
+
+function checkKeyEvent(event: KeyboardEvent): void {
+  if (RELEVANT_KEYBOARD_EVENTS.includes(event.code)) {
+    updateWord();
+  }
 }
