49597e12b3ffda61e30999e75b116ea956bb03cb
Euan Goddard <euan@stockopedia.com>
2019-03-02 12:58:36 +0000

    Updates

---
+++ sw.js
@@ -0,0 +1,43 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.0.0/workbox-sw.js');
+workbox.precaching.precacheAndRoute([
+  {
+    "url": "android-chrome-192x192.6c6655d6.png",
+    "revision": "53ffcd7b81158f9f50b93759ed662af6"
+  },
+  {
+    "url": "android-chrome-512x512.b7f5c563.png",
+    "revision": "e2a3ddb9cf26ba45d55ee5849ab85fd1"
+  },
+  {
+    "url": "apple-touch-icon.2326d736.png",
+    "revision": "fdfaddca21b07af4167a08edd3c298b3"
+  },
+  {
+    "url": "favicon-16x16.913f0ed5.png",
+    "revision": "fac6993fee00ee8385fb6d63433d66bc"
+  },
+  {
+    "url": "favicon-32x32.607f18bf.png",
+    "revision": "f6ede074099e692e24debd01d8469301"
+  },
+  {
+    "url": "index.html",
+    "revision": "9e81565ecc126242505026905d3e5dfc"
+  },
+  {
+    "url": "manifest.webmanifest",
+    "revision": "875a820840b2124ba10149ad4d1b0a2d"
+  },
+  {
+    "url": "src.16165790.js",
+    "revision": "4ee8e2861f1e5bd30500595c229299df"
+  },
+  {
+    "url": "style.4baa0d9a.css",
+    "revision": "c91c928adbd2849943cf56d6a00bda71"
+  },
+  {
+    "url": "sw-register.ea91e91b.js",
+    "revision": "5f454be24555de150552baa829739c55"
+  }
+]);
