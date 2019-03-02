621152fc55f414594ae5326ee207e9a8eeb042c4
Euan Goddard <euan@stockopedia.com>
2019-03-02 13:06:37 +0000

    Added service worker

---
+++ src/sw.js
@@ -0,0 +1,2 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.0.0/workbox-sw.js');
+workbox.precaching.precacheAndRoute([]);
---
+++ workbox-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "globDirectory": "dist/",
+  "globPatterns": [
+    "**/*.{png,html,webmanifest,js,css}"
+  ],
+  "swDest": "dist/sw.js",
+  "swSrc": "src/sw.js"
+};
